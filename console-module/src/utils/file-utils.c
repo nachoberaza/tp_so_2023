d8d6e2df318618c0cb86c283321a61f12fbfbfd5
Mateo Fryda Saez <frydasaezmateo@gmail.com>
2023-04-13 04:43:30 +0000

    recieve commands from file instead of console

--- console-module/src/utils/file-utils.c
+++ console-module/src/utils/file-utils.c
@@ -1,14 +1,21 @@
 #include "file-utils.h"
 
-void read_console_commands(t_package* package) {
-	char* readLine;
-	int size;
+void fill_package_from_file(t_package *package){
+	FILE * filePointer;
+	char * line = NULL;
+	size_t length = 0;
+	ssize_t read;
 
-	readLine = readline("> ");
-	do {
-		size = strlen(readLine) + 1;
-		fill_buffer(package, readLine, size);
+	filePointer = fopen("console.code", "r");
+	if (filePointer == NULL)
+		exit(EXIT_FAILURE);
 
-		readLine = readline("> ");
-	}while (strcmp(readLine, "\0"));
+	while ((read = getline(&line, &length, filePointer)) != -1) {
+		write_to_log(LOG_TARGET_INTERNAL, LOG_LEVEL_DEBUG, string_from_format("Linea: %s", line));
+		fill_package(package, line, read);
+	}
+
+	fclose(filePointer);
+	if (line)
+		free(line);
 }
