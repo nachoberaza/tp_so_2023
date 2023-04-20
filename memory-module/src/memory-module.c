c3d079c8331d0abb71d51e9530812fd2456c679c
Mateo Fryda Saez <frydasaezmateo@gmail.com>
2023-04-20 01:04:55 +0000

    Threads WIP, needs A LOT of love

--- memory-module/src/memory-module.c
+++ memory-module/src/memory-module.c
@@ -1,5 +1,7 @@
 #include "memory-module.h"
 
+pthread_t tid[2];
+
 int main(void) {
 	t_memory_config *MEMORY_ENV = create_memory_config(MODULE_NAME);
 	init_logger(MODULE_NAME, MEMORY_ENV->LOG_LEVEL);
@@ -10,10 +12,37 @@ int main(void) {
 
 	write_to_log(LOG_TARGET_ALL, LOG_LEVEL_INFO, "Servidor listo para recibir al cliente");
 
-	int clientSocketId = await_client(get_logger(), serverSocketId);
 
+	for	(int i=0;i<3;i++)
+	{
+		write_to_log(LOG_TARGET_ALL, LOG_LEVEL_INFO, string_from_format("Inside %d", i));
+		int clientSocketId = await_client(get_logger(), serverSocketId);
+		write_to_log(LOG_TARGET_ALL, LOG_LEVEL_INFO, string_from_format("Me llego algo: %d", clientSocketId));
+
+	    int err = pthread_create(&(tid[i]), NULL, handleConnection, (void*) &clientSocketId);
+
+	    if (err != 0){
+	    	printf("\nHubo un problema al crear el thread :[%s]", strerror(err));
+	    	return err;
+	    }
+	}
+
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_join(tid[2], NULL);
+
+	return EXIT_SUCCESS;
+}
+
+void* handleConnection(void *clientSocket)
+{
+    int clientSocketId = *(int*)clientSocket;
 	t_list *commands;
-	while (1) {
+	write_to_log(LOG_TARGET_ALL, LOG_LEVEL_INFO, string_from_format("clientsocket inside handle: %d", clientSocketId));
+
+	int flag = 1;
+
+	while (flag) {
 		int operationCode = receive_operation_code(clientSocketId);
 		switch (operationCode) {
 		case MESSAGE:
@@ -21,17 +50,17 @@ int main(void) {
 			break;
 		case PACKAGE:
 			commands = decode_package(clientSocketId);
-			write_to_log(LOG_TARGET_ALL, LOG_LEVEL_INFO, "Me llegaron los siguientes valores:\n");
+			write_to_log(LOG_TARGET_ALL, LOG_LEVEL_INFO, string_from_format("clientSocket %d, Me llegaron los siguientes valores:\n", clientSocketId));
 			list_iterate(commands, (void*) write_info_to_all_logs);
 			break;
 		case -1:
 			write_to_log(LOG_TARGET_ALL, LOG_LEVEL_ERROR, "el cliente se desconecto. Terminando servidor");
-			return EXIT_FAILURE;
+			flag = 0;
+			break;
 		default:
 			write_to_log(LOG_TARGET_ALL, LOG_LEVEL_WARNING, "Operacion desconocida. No quieras meter la pata");
 			break;
 		}
 	}
 
-	return EXIT_SUCCESS;
 }
