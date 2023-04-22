813fbea4ec1fa60cfccede5bb7e34665e0d6a5dc
Maria Victoria Otero <vicucha4@gmail.com>
2023-04-22 19:53:36 +0000

    Add pcb

--- kernel-module/src/kernel-module.c
+++ kernel-module/src/kernel-module.c
@@ -22,6 +22,10 @@ int main(void) {
 			break;
 		case PACKAGE:
 			commands = decode_package(clientSocketId);
+			t_list* instructions= create_instruction_list(commands);
+			t_pcb* pcb = create_pcb(instructions);
+			write_to_log(LOG_TARGET_INTERNAL, LOG_LEVEL_INFO, "Instrucciones en el PCB:\n");
+			list_iterate(pcb->instructions, (void*) write_intructions_to_all_logs);
 			write_to_log(LOG_TARGET_INTERNAL, LOG_LEVEL_INFO, "Me llego el siguiente package:\n");
 			list_iterate(commands, (void*) write_info_to_all_logs);
 			t_package* packageReceived = build_package(commands);
@@ -42,3 +46,72 @@ int main(void) {
 
 	return EXIT_SUCCESS;
 }
+
+
+t_pcb* new(void){
+	t_pcb *pcb = malloc(sizeof(t_pcb));
+
+	pcb->instructions = list_create();
+	pcb->segment_table = list_create();
+	pcb->open_files_table = list_create();
+	pcb->state=NEW;
+
+	return pcb;
+}
+
+t_pcb* create_pcb(t_list* intructions){
+	t_pcb *pcb = new();
+
+	pcb->instructions=intructions;
+
+	return pcb;
+}
+
+t_list* create_instruction_list(t_list* userInput){
+	t_list* instructions = list_create();
+
+	for (int i=0; i< list_size(userInput);i++){
+		t_kernel_instruction * inst = create_instruction(list_get(userInput,i));
+		list_add(instructions, inst);
+	}
+
+	return instructions;
+}
+
+t_kernel_instruction* create_instruction(char* instruction){
+	t_kernel_instruction * inst = malloc(sizeof(t_kernel_instruction));
+	inst->parameters = list_create();
+
+	char** split = string_split(instruction," ");
+	inst->command= SET;//cambiar
+	for (int i=1; i< string_array_size(split);i++){
+		list_add(inst->parameters, split[i]);
+	}
+
+	return inst;
+}
+
+
+void add_intruction(t_pcb* pcb, t_kernel_instruction* instruction){
+	list_add(pcb->instructions,instruction);
+}
+
+void add_segment(t_pcb* pcb, t_segment_row* segment){
+	list_add(pcb->segment_table,segment);
+}
+
+void add_file(t_pcb* pcb, t_open_file_row* openFile){
+	list_add(pcb->open_files_table,openFile);
+}
+
+void free_pcb(t_pcb* pcb){
+	//TODO: crear funcion que destruya elementos
+	//list_destroy_and_destroy_elements(pcb,funcionDestructiva)
+}
+
+void write_intructions_to_all_logs(t_kernel_instruction* instruction) {
+
+	write_info_to_all_logs("SET");
+	list_iterate(instruction->parameters, (void*) write_info_to_all_logs);
+}
+
