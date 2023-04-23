a97ca02df6e9500ee5ee8c075dc53612bd973c29
Mateo Fryda Saez <frydasaezmateo@gmail.com>
2023-04-23 19:58:05 +0000

    Changed file variable names

--- console-module/src/utils/file-utils.c
+++ console-module/src/utils/file-utils.c
@@ -3,20 +3,20 @@
 void fill_package_from_file(t_package *package){
 	FILE * file;
 	char * line = NULL;
-	size_t length = 0;
-	ssize_t lineValue;
+	size_t tSize = 0;
+	ssize_t length;
 
 	file = fopen("console.code", "r");
 	if (file == NULL)
 		exit(EXIT_FAILURE);
 
-	while ((lineValue = getline(&line, &length, file)) != -1) {
+	while ((length = getline(&line, &tSize, file)) != -1) {
 		if (string_ends_with(line,"\n")){
-			line = string_substring(line,0,lineValue-1);
+			line = string_substring(line,0,length-1);
 		}
 
 		write_to_log(LOG_TARGET_INTERNAL, LOG_LEVEL_DEBUG, string_from_format("Line value: %s", line));
-		fill_buffer(package, line, lineValue);
+		fill_buffer(package, line, length);
 	}
 
 	fclose(file);
