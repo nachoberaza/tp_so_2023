adae74d65dd6f430d594251be69fc701ef8a0499
Maria Victoria Otero <vicucha4@gmail.com>
2023-07-15 17:01:55 +0000

    me da lo mismo

--- cpu-module/Debug/src/utils/subdir.mk
+++ cpu-module/Debug/src/utils/subdir.mk
@@ -32,7 +32,7 @@ OBJS += \
 src/utils/%.o: ../src/utils/%.c src/utils/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/Documents/dev/tp-2023-1c-Compu-mundo-hiper-mega-red/shared-library" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	gcc -I"/home/utnso/Documents/TP-Tuki/tp-2023-1c-Compu-mundo-hiper-mega-red/shared-library" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
